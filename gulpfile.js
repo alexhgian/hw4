c447a60cc948a6c1dcb55f932f18ca87660dc2a9
Alex Gian <alexhgian@gmail.com>
2015-05-25 15:50:27 -0700

    gulp live server

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+
+gulp.task('serve', function() {
+    // serve with default settings
+    var server = gls.static(['.','bower_components'],3000); //equals to gls.static('public', 3000);
+    server.start();
+
+    //use gulp.watch to trigger server actions(notify, start or stop)
+    gulp.watch(['js/**/*.js','style/**/*.css', 'view/**/*.html'], server.notify);
+});
+
+
+// Run all task before running 'default'
+gulp.task('default',['serve']);
