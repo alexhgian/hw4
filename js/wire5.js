38aaa958b96ffc9ed07e903828fd026a3d36b1be
Alex Gian <alexhgian@gmail.com>
2015-05-31 23:07:17 -0700

    Image Required Alert

--- js/wire5.js
+++ js/wire5.js
@@ -10,7 +10,7 @@ function triggerFunction() {
 
 //formatting the date
 function dateToString(date) {
-  var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June',
+    var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December' ];
 
     var month = date.getMonth() ;
@@ -23,40 +23,47 @@ function dateToString(date) {
 
 //this function lets to preview the image
 var loadFile = function(event) {
-  var reader = new FileReader();
-  reader.onload = function(){
-    var output = document.getElementById('output');
-    output.src = reader.result;
-  };
-  reader.readAsDataURL(event.target.files[0]);
+    var reader = new FileReader();
+    reader.onload = function(){
+        var output = document.getElementById('output');
+        output.src = reader.result;
+        var imgReq = document.getElementsByClassName('coin_image-required')[0];
+        imgReq.style.visibility = "hidden";
+    };
+    reader.readAsDataURL(event.target.files[0]);
 };
 
 
 
 // displays current date
 window.onload = function () {
+    var imgReq = document.getElementsByClassName('coin_image-required')[0];
+    imgReq.style.visibility = "hidden";
 
-  var currentdate = new Date();
-  var datetime= '';
-  datetime += dateToString(currentdate );
-  document.getElementById('datepicker').value = datetime;
-  getparseData();
+    var currentdate = new Date();
+    var datetime= '';
+    datetime += dateToString(currentdate );
+    document.getElementById('datepicker').value = datetime;
+    getparseData();
 
-//uplaod to server parse
-  document.getElementById("save")
+    //uplaod to server parse
+    document.getElementById("save")
     .addEventListener("click", function() {
         var input = document.getElementById('file_upload');
         var file = input.files[0];
+        if(!file){
+            var imgReq = document.getElementsByClassName('coin_image-required')[0];
+            imgReq.style.visibility = "visible";
+        }
         var serverUrl = 'https://api.parse.com/1/files/' + file.name;
         var req = new XMLHttpRequest();
 
         req.onreadystatechange = function(oEvent) {
             if (req.readyState === 4) {
                 if (req.status === 201) { // Handle Success and Failure
-                  // Parse the string into a JSON obj
-                  console.log('Success: '+req.responseText);
-                  successUpload(req.responseText);
-
+                    // Parse the string into a JSON obj
+                    console.log('1Success: '+req.responseText);
+                    successUpload(req.responseText);
                 } else {
 
                     console.log("Error: ", req.statusText); // Error Message
@@ -70,72 +77,72 @@ window.onload = function () {
         req.setRequestHeader("Content-Type", file.type);
         req.send(file);
     });
- };
+};
 
 
 //associating image with the class
 function successUpload(data) {
-  var qty = parseFloat(document.getElementById('qty').value);
-  var item =  document.getElementById('item').value;
-  var metal = document.getElementById('metal').value;
-  var premium = parseFloat(document.getElementById('premium').value);
-  var percent = parseFloat(document.getElementById('percent').innerHTML);
-  var weight = parseFloat(document.getElementById('weight').innerHTML);
+    var qty = parseFloat(document.getElementById('qty').value);
+    var item =  document.getElementById('item').value;
+    var metal = document.getElementById('metal').value;
+    var premium = parseFloat(document.getElementById('premium').value);
+    var percent = parseFloat(document.getElementById('percent').innerHTML);
+    var weight = parseFloat(document.getElementById('weight').innerHTML);
 
-  var bodyData = {
-      item : item,
-      metal: metal.toLowerCase(),
-      quantity: qty,
-      premium: premium,
-      percent: percent,
-      weight: weight,
-      image : {
-              'name': JSON.parse(data).name,
-              '__type': 'File'
-      }
-  }
+    var bodyData = {
+        item : item,
+        metal: metal.toLowerCase(),
+        quantity: qty,
+        premium: premium,
+        percent: percent,
+        weight: weight,
+        image : {
+            'name': JSON.parse(data).name,
+            '__type': 'File'
+        }
+    }
 
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function(oEvent) {
-      if (req.readyState === 4) {
-          if (req.status === 201) { // Handle Success and Failure
-              console.log('Success');
-              document.location.href=("wire3.html");
-          } else {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(oEvent) {
+        if (req.readyState === 4) {
+            if (req.status === 201) { // Handle Success and Failure
+                console.log('Success');
+                document.location.href=("wire3.html");
+            } else {
 
-              console.log("Error: ", req.statusText); // Error Message
-          }
-      }
-  };
-  req.open("POST", " https://api.parse.com/1/classes/coin", true);
-  req.setRequestHeader("X-Parse-Application-Id", appId);
-  req.setRequestHeader("X-Parse-REST-API-Key", apiKey);
-  req.setRequestHeader("Content-Type", 'application/json');
-  req.send(JSON.stringify(bodyData));
+                console.log("Error: ", req.statusText); // Error Message
+            }
+        }
+    };
+    req.open("POST", " https://api.parse.com/1/classes/coin", true);
+    req.setRequestHeader("X-Parse-Application-Id", appId);
+    req.setRequestHeader("X-Parse-REST-API-Key", apiKey);
+    req.setRequestHeader("Content-Type", 'application/json');
+    req.send(JSON.stringify(bodyData));
 };
 
 //getting parse data from info class
 function getparseData () {
-  var req1 = new XMLHttpRequest();
-  req1.onreadystatechange = function(oEvent) {
-      if (req1.readyState === 4) {
-          if (req1.status === 200) { // Handle Success and Failure
-            // Parse the string into a JSON obj
-            var tmpObj1 = JSON.parse(req1.responseText);
-            console.log('Success');
-            tmpObj = tmpObj1.results;
-            populateDropdown(tmpObj1.results);
+    var req1 = new XMLHttpRequest();
+    req1.onreadystatechange = function(oEvent) {
+        if (req1.readyState === 4) {
+            if (req1.status === 200) { // Handle Success and Failure
+                // Parse the string into a JSON obj
+                var tmpObj1 = JSON.parse(req1.responseText);
+                console.log('Success');
+                tmpObj = tmpObj1.results;
+                populateDropdown(tmpObj1.results);
 
-          } else {
-            console.log("Error: ", req1.statusText); // Error Message
-          }
-      }
-  };
-  req1.open("GET", "https://api.parse.com/1/classes/Info", true);
-  req1.setRequestHeader("X-Parse-Application-Id", appId);
-  req1.setRequestHeader("X-Parse-REST-API-Key", apiKey);
-  req1.setRequestHeader("Content-Type", 'application/json');
-  req1.send();
+            } else {
+                console.log("Error: ", req1.statusText); // Error Message
+            }
+        }
+    };
+    req1.open("GET", "https://api.parse.com/1/classes/Info", true);
+    req1.setRequestHeader("X-Parse-Application-Id", appId);
+    req1.setRequestHeader("X-Parse-REST-API-Key", apiKey);
+    req1.setRequestHeader("Content-Type", 'application/json');
+    req1.send();
 };
 
 
@@ -173,30 +180,30 @@ function populateDropdown(tmpObj) {
     });
 
     var currentObj = goldObj;
-          function updateValues() {
-         var coinInfoEl  = getCoinInfo(typeEl.value, currentObj);
-         var fineness = parseFloat(coinInfoEl.weight)*parseFloat(coinInfoEl.percent);
-         percent.innerHTML = coinInfoEl.percent;
-         weight.innerHTML = coinInfoEl.weight;
-         goldo.innerHTML =  (fineness).toFixed(3);
-         goldTotal.innerHTML = (parseFloat(qty.value) * fineness).toFixed(3);
-         total.innerHTML = (price * parseFloat(qty.value) * fineness).toFixed(3);
-         totalPremium.innerHTML = ((price * parseFloat(qty.value) * fineness) + parseFloat(premium.value)).toFixed(3);
-       }
+    function updateValues() {
+        var coinInfoEl  = getCoinInfo(typeEl.value, currentObj);
+        var fineness = parseFloat(coinInfoEl.weight)*parseFloat(coinInfoEl.percent);
+        percent.innerHTML = coinInfoEl.percent;
+        weight.innerHTML = coinInfoEl.weight;
+        goldo.innerHTML =  (fineness).toFixed(3);
+        goldTotal.innerHTML = (parseFloat(qty.value) * fineness).toFixed(3);
+        total.innerHTML = (price * parseFloat(qty.value) * fineness).toFixed(3);
+        totalPremium.innerHTML = ((price * parseFloat(qty.value) * fineness) + parseFloat(premium.value)).toFixed(3);
+    }
     qty.onkeyup = updateValues;
     premium.onkeyup = updateValues;
     // Listiners
     metalEl.onchange = function() {
         switch (this.value) {
             case 'Gold':
-                currentObj = goldObj;
-                break;
+            currentObj = goldObj;
+            break;
             case 'Silver':
-                currentObj = silverObj;
-                break;
+            currentObj = silverObj;
+            break;
             case 'Platinum':
-                currentObj = platinumObj;
-                break;
+            currentObj = platinumObj;
+            break;
         }
         while (typeEl.firstChild) {
             typeEl.removeChild(typeEl.firstChild);
@@ -209,17 +216,17 @@ function populateDropdown(tmpObj) {
         });
 
 
-          coinInfoEl = getCoinInfo(typeEl.value, currentObj);
-          fineness = parseFloat(coinInfoEl.weight)*parseFloat(coinInfoEl.percent);
-          percent.innerHTML = coinInfoEl.percent;
-          weight.innerHTML = coinInfoEl.weight;
-          goldo.innerHTML =  (fineness).toFixed(3);
-          goldTotal.innerHTML = (parseFloat(qty.value) * fineness).toFixed(3);
-          total.innerHTML = (price * parseFloat(qty.value) * fineness).toFixed(3);
-          totalPremium.innerHTML = ((price * parseFloat(qty.value) * fineness) + parseFloat(premium.value)).toFixed(3) ;
+        coinInfoEl = getCoinInfo(typeEl.value, currentObj);
+        fineness = parseFloat(coinInfoEl.weight)*parseFloat(coinInfoEl.percent);
+        percent.innerHTML = coinInfoEl.percent;
+        weight.innerHTML = coinInfoEl.weight;
+        goldo.innerHTML =  (fineness).toFixed(3);
+        goldTotal.innerHTML = (parseFloat(qty.value) * fineness).toFixed(3);
+        total.innerHTML = (price * parseFloat(qty.value) * fineness).toFixed(3);
+        totalPremium.innerHTML = ((price * parseFloat(qty.value) * fineness) + parseFloat(premium.value)).toFixed(3) ;
 
-         qty.onkeyup = updateValues;
-         premium.onkeyup = updateValues;
+        qty.onkeyup = updateValues;
+        premium.onkeyup = updateValues;
     };
 
     typeEl.onchange = function() {
